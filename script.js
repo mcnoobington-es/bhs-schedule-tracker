a6c43c0f82c799f373f683c083e83eb2b22d075e
mcnoobington-es <isaacdavidm0516@gmail.com>
2025-09-23 10:35:03 +0200

    supposedly fixed json bugs

---
+++ script.js
@@ -0,0 +1,5 @@
+const whatDay = document.getElementById("whatDay");
+const whatPeriod = document.getElementById("whatPeriod");
+const nextPeriod = document.getElementById("nextPeriod");
+const divider = document.getElementById("divider");
+
